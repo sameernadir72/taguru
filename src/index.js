bdd3006a67f33d011efe919ff6338f14357a7aa8
sameernadir72 <sameernadir72@gmail.com>
2023-03-28 13:04:01 +0500

    'changes'

--- src/index.js
+++ src/index.js
@@ -12,17 +12,25 @@ import { store } from 'store';
 // style + assets
 import 'assets/scss/style.scss';
 import config from './config';
-
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 // ==============================|| REACT DOM RENDER  ||============================== //
 
+function getLibrary(provider) {
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
+}
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-    <Provider store={store}>
-        <BrowserRouter basename={config.basename}>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+        <Provider store={store}>
+            <BrowserRouter basename={config.basename}>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </Web3ReactProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
