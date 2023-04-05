d06579c7f02c74ba80ff3b77a5563fa9f5e81090
raghav <raghavjindal0212@gmail.com>
2023-04-05 22:20:00 +0530

    eslnit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 // project imports
-import * as serviceWorker from "serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 import App from "App";
 import { store } from "store";
 
